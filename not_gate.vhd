74168a55396106c93469dbb34c8b141646d54366
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-25 15:19:05 +0530

    Create not_gate.vhd

---
+++ not_gate.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+entity not_gate is
+    Port ( a : in  STD_LOGIC;
+           b : out  STD_LOGIC);
+end not_gate;
+architecture Behavioral of not_gate is
+begin
+process(a)
+begin
+       b<=not a;
+end process;
+end Behavioral;
