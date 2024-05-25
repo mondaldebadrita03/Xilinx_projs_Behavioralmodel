650729972016c0a02ceb28ddc211862640f23406
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-25 17:58:30 +0530

    Update 8to1mux.vhd

--- 8to1mux.vhd
+++ 8to1mux.vhd
@@ -3,8 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity 8to1mux is
-    Port ( I : in  STD_LOGIC_VECTOR (1 downto 0);
-           S : in  STD_LOGIC;
+    Port ( I : in  STD_LOGIC_VECTOR (7 downto 0);
+           S : in  STD_LOGIC_VECTOR (3 downto 0);
            Y : out  STD_LOGIC);
 end 8to1mux;
 architecture Behavioral of 8to1mux is
