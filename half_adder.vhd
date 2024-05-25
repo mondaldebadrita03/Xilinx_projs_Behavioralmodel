3aed5b636ff7d3dd9b11af7310d64863b6ea228e
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-25 15:33:31 +0530

    Create half_adder.vhd

---
+++ half_adder.vhd
@@ -0,0 +1,26 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+entity half_adder is
+    Port ( a : in  STD_LOGIC;
+           b : in  STD_LOGIC;
+           s : out  STD_LOGIC;
+           cout : out  STD_LOGIC);
+end half_adder;
+architecture Behavioral of half_adder is
+begin
+process(a,b)
+begin
+       if((a='1') and (b='1')) or ((a='0') and (b='0')) then
+              s<='0';
+        else
+              s<='1';
+        end if;
+       if(a='1' and b='1') then
+             cout<='1';
+        else
+             cout<='0';
+        end if;
+end process;
+end Behavioral;
