03b11e3e47e048d183e46d0ce6b103d6b3bff952
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-25 15:10:24 +0530

    Create and_gate.vhd

---
+++ and_gate.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+entity and_gate is
+    Port ( a : in  STD_LOGIC;
+           b : in  STD_LOGIC;
+           c : out  STD_LOGIC);
+end and_gate;
+architecture Behavioral of and_gate is
+begin
+process(a,b)
+begin
+       if(a='1') and (b='1') then
+              c<='0';
+        else
+              c<='1';
+        end if;
+end process;
+end Behavioral;
