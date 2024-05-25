80a72846f9b65c873759ef83d3449ada5ba561cc
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-25 17:41:58 +0530

    Create 2to1mux.vhd

---
+++ 2to1mux.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+entity 2to1mux is
+    Port ( I : in  STD_LOGIC_VECTOR (1 downto 0);
+           S : in  STD_LOGIC;
+           Y : out  STD_LOGIC);
+end 2to1mux;
+architecture Behavioral of 2to1mux is
+begin
+process(I)
+begin
+        if(S='1') then
+              Y<= I(1);
+        else
+              Y<=I(0);
+        end if;
+    end process;
+end Behavioral;
